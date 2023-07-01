ddb5709e11d1866b7916f2b010b1b6f6a953b6d0
MrGoopyDrawers <107856947+MrGoopyDrawers@users.noreply.github.com>
2023-07-01 19:36:11 -0400

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+namespace BedwarsTowers;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "37.3";
+    public const string Version = "1.0.0";
+    public const string Name = "BedwarsTowers";
+
+    public const string Description = "These are from roblox bedwars kits";
+
+    public const string RepoOwner = "MrGoopyDrawers"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "bedwars"; // TODO add your repo name here, also in the download url in README.md
+}
